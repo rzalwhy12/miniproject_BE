f9ff11a19e9b298423257e3907f3ff83cb17bb89
rzalwhy12 <rzalcorp05@gmail.com>
2025-08-22 18:03:42 +0700

    vercelll

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,5 @@ import App from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Create instance of App
 const app = new App();
-
-// For Vercel, we export the Express app
-const handler = app.app;
-
-// Start the server if we're running locally
-if (!process.env.VERCEL) {
-  app.start();
-}
-
-export default handler;
+app.start();
