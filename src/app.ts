3025d3f9e57f32ace1667f37e5544c2db6c581a7
rzal <rzalcorp05@gmail.com>
2025-08-22 22:12:47 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ class App {
   private configure = (): void => {
     // Configure CORS for production
     this.app.use(cors({
-      origin: process.env.BASIC_URL_FE && "http://localhost:3000",
+      origin: process.env.BASIC_URL_FE || "http://localhost:3000",
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
       allowedHeaders: ["Content-Type", "Authorization"]
